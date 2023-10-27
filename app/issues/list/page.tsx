bd5e2d96e90ba0203f65f994b2ca280d8a454553
victor-levus <95586250+victor-levus@users.noreply.github.com>
2023-10-27 19:35:36 +0100

    new commit

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -54,7 +54,10 @@ const IssuesPage = async ({ searchParams }: Props) => {
         <Table.Header>
           <Table.Row>
             {columns.map((column) => (
-              <Table.ColumnHeaderCell key={column.label}>
+              <Table.ColumnHeaderCell
+                key={column.label}
+                className={column.className}
+              >
                 <NextLink
                   href={{ query: { ...searchParams, orderBy: column.value } }}
                 >
